aeeb72df30d12878fd8c253d37d60b70bb397ba1
Al Haseeb Zafar <94920539+ALHaseebZafar@users.noreply.github.com>
2024-12-27 15:36:56 +0500

    sidebar changes

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import Header from '../components/Header';  // Import Header component
-import Sidebar from '../components/Sidebar';
-function Layout({children}) {
+import React from "react";
+import Header from "../components/Header"; // Import Header component
+import Sidebar from "../components/Sidebar";
+function Layout({ children }) {
   return (
     <div>
-      <Header />  {/* Add the Header component */}
-      <div className="w-full flex ">
-        <div className="max-w-max">
-      <Sidebar/>
+      <Header /> {/* Add the Header component */}
+      <div className="w-full flex min-h-screen ">
+        <div className="max-w-max ">
+          <Sidebar />
+        </div>
+        <div className="w-[80%]">{children}</div>
       </div>
-      <div className='w-[80%]'>
-        {children}
-      </div>
-      </div>
-    
     </div>
   );
 }
-
-export default Layout
+export default Layout;
